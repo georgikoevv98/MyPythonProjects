1d28ac46955ce94c9995940687487b03fba5a21a
unknown <georgikoevv98@gmail.com>
2025-07-18 14:57:00 +0300

    Initial commit

---
+++ madlibs.py
@@ -0,0 +1,10 @@
+# Mad Libs Game
+adj = input("Adjective: ")
+verb1 = input("Verb: ")
+verb2 = input("Verb: ")
+famous_person = input("Famous Person: ")
+
+madlib = f"Learnning Python is so {adj}! It makes me so excited because \
+I love to {verb1} new things and {verb2} like you are {famous_person}!"
+
+print(madlib) 
